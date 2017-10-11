c7e9f5c8737c5a99664f1b95af7939bdaf155341
HP <HP@192.168.0.152>
2017-10-11 18:59:41 +0900

    //login 완료

--- Bookstagram/src/com/kosta/bookstagram/controller/LoginController.java
+++ Bookstagram/src/com/kosta/bookstagram/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.kosta.bookstagram.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.kosta.bookstagram.controller.listener.Controller;
 import com.kosta.bookstagram.model.MemberDAO;
@@ -10,21 +11,18 @@ import com.kosta.bookstagram.model.MemberVO;
 public class LoginController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = null;
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		MemberVO member = MemberDAO.getInstance().checkMember(id, pw);
 		
 		if(member != null) {
 			System.out.println("Login Session Ok !");
-			
-			
-			
+			session = request.getSession();
+			session.setAttribute("member", member);
 			return "login:success";
 		}else {
 			System.out.println("Login and Session Failed");
-			
-			
-			
 			return "login:fail";
 		}
 		
