5e97068c036444710456f7154275a1df68b9cc42
hwangseichang <databas2@naver.com>
2017-10-14 16:51:25 +0900

    cms member board 처리

--- Bookstagram/src/com/kosta/bookstagram/controller/CmsBoardListPageController.java
+++ Bookstagram/src/com/kosta/bookstagram/controller/CmsBoardListPageController.java
@@ -4,13 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kosta.bookstagram.controller.listener.Controller;
+import com.kosta.bookstagram.model.CmsDAO;
 
 public class CmsBoardListPageController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		request.setAttribute("board", CmsDAO.getInstance().getAllBoardList());
+		request.setAttribute("url", "cms_board_list.jsp");
+		return "home.jsp";
 	}
 
 }
--- Bookstagram/src/com/kosta/bookstagram/controller/CmsMemberListPageController.java
+++ Bookstagram/src/com/kosta/bookstagram/controller/CmsMemberListPageController.java
@@ -4,13 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kosta.bookstagram.controller.listener.Controller;
+import com.kosta.bookstagram.model.CmsDAO;
 
 public class CmsMemberListPageController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		return null;
+		request.setAttribute("member", CmsDAO.getInstance().getAllMemberList());
+		request.setAttribute("url", "cms_member_list.jsp");
+		return "home.jsp";
 	}
 
 }
