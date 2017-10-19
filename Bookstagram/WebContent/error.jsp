64064f501e18bdf4991e07100c9e33d2ded7ae69
HP <HP@192.168.0.152>
2017-10-19 13:06:54 +0900

    탈퇴회원 글쓰기 처리

---
+++ Bookstagram/WebContent/error.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=UTF-8>
+<title>Insert title here</title>
+</head>
+<body>
+<script type="text/javascript">
+	alert("시스템관리자로 인해 서비스가 중지된회원 입니다.");
+	location.href="DispatcherServlet?command=Logout";
+</script>
+</body>
+</html>
--- Bookstagram/src/com/kosta/bookstagram/controller/CreationPostWriteViewController.java
+++ Bookstagram/src/com/kosta/bookstagram/controller/CreationPostWriteViewController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.kosta.bookstagram.controller.listener.Controller;
 import com.kosta.bookstagram.model.BoardVO;
+import com.kosta.bookstagram.model.CmsDAO;
 import com.kosta.bookstagram.model.CreateBoardDAO;
 import com.kosta.bookstagram.model.CreateCategoryVO;
+import com.kosta.bookstagram.model.MemberVO;
 
 public class CreationPostWriteViewController implements Controller {
 
@@ -16,9 +19,17 @@ public class CreationPostWriteViewController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ArrayList<BoardVO> bglist=CreateBoardDAO.getInstance().boardBackground(); 
 		ArrayList<CreateCategoryVO> categoryList=CreateBoardDAO.getInstance().createCategory();
-		request.setAttribute("bglist", bglist);
-		request.setAttribute("category", categoryList);
-		request.setAttribute("url", "creation_write.jsp");
+		HttpSession session = request.getSession();
+		MemberVO member = (MemberVO)session.getAttribute("member");
+		int member_tier = CmsDAO.getInstance().getMemberTier(member.getId());
+		if(member_tier != 0) {
+			request.setAttribute("bglist", bglist);
+			request.setAttribute("category", categoryList);
+			request.setAttribute("url", "creation_write.jsp");
+		}else {
+			return "error.jsp";
+		}
+		
 		return "home.jsp";
 	}
 
